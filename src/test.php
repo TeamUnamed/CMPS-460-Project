9a2cb82ff2d7354b28520b880dc56c06079a9f0c
malfact <themalfact@gmail.com>
2023-04-25 18:16:49 -0500

    It now functions

--- src/test.php
+++ src/test.php
@@ -1,3 +1,30 @@
 <?php
-    echo "Hello World\n<h2>HELLO</h2>";
-    echo "GO AWAY";
+
+    echo "<h1>TESTING SQL TABLE STUFF</h1>";
+
+    $servername = "localhost";
+    $username = "root";
+    $password = "";
+    $dbname = "test_db";
+
+    function getDatabaseConnection($servername, $username, $password, $dbname) {
+        try {
+            $conn = new mysqli($servername, $username, $password);
+        } catch (mysqli_sql_exception $error) {
+            echo "Connection Failed:", $error ->getCode(), "<br>";
+            echo $error->getMessage(), "<br>";
+            echo $error->getFile(), " [", $error->getLine(), "]";
+            die;
+        }
+
+        if ($conn -> select_db($dbname)) {
+            return $conn;
+        }
+
+        $conn -> query("CREATE DATABASE " . $dbname);
+        $conn -> select_db($dbname);
+        return $conn;
+    }
+
+    $conn = getDatabaseConnection($servername, $username, $password, $dbname);
+    echo $conn -> ping();
