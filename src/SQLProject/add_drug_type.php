6cda4c9ba73d5ea91f5bddd115631693392d866e
malfact <themalfact@gmail.com>
2023-04-26 20:56:47 -0500

    Add Drug Type

---
+++ src/SQLProject/add_drug_type.php
@@ -0,0 +1,53 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php
+    include "util/FormUtil.php";
+    include "util/SQLConnection.php";
+    include "generic/header_generic.html";
+
+    use SQLProject\util\SQLConnection;
+    ?>
+    <title>Add Drug Type</title>
+</head>
+<body>
+<p>
+<form method="post" actions="<?php echo htmlspecialchars($_SERVER['PHP_SELF']) ?>">
+    <?php
+    input('number', 'type_id', 'Type ID');
+    input('number', 'drug_id', 'Drug ID');
+    input('text', 'type_desc', 'Type Description');
+    ?> <br>
+    <input type="submit" value="Add">
+</form>
+</p>
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+    print "<p>";
+    print "Adding Drug Type: <b>"
+        . getData("type_name") . " ("
+        . getData('type_id') .  ")("
+        . getData('drug_id') . ")</b><br>";
+    $conn = new SQLConnection(SERVER, USERNAME, PASSWORD);
+    $conn->connect();
+    $result = $conn->selectDatabase(DATABASE, true);
+    print "Connected:: " . ($result ? "Yes" : "No") . "<br>";
+
+    $result = $conn->query("SELECT id FROM Drugs WHERE id=" . getData('drug_id'));
+
+    if (!$result || $result -> num_rows == 0) {
+        print "Added:: No -> No Matching Drug ID";
+    } else {
+        $result = $conn->query("INSERT INTO Drug_Types VALUES (" .
+            getData('type_id') . ", " .
+            getData('drug_id') . ", '" .
+            getData('type_desc') . "')"
+        );
+
+        print "Added:: " . ($result ? "Yes" : "No");
+        print "</p>";
+    }
+}
+?>
+</body>
+</html>
